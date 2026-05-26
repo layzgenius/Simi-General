5f9df6f57919c963d32c4339823cb5594283c7ac
layzgenius <lazyscholarph@gmail.com>
2026-05-26 21:32:54 +0800

    Initial Commit

---
+++ Simi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simi
+//
+//  Created by Steven Anthony Heras Manahan on 5/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simi/SimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimiApp.swift
+//  Simi
+//
+//  Created by Steven Anthony Heras Manahan on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
